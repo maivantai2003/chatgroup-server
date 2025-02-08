81327a03edbcbc0003a7124eddca3a4c2e768cb0
Van Tai <vantai08122003@gmail.com>
2025-02-08 22:17:15 +0700

    install package important in project

---
+++ chatgroup-server/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace chatgroup_server.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) { }
+    }
+}
