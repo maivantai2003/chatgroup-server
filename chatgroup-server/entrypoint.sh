8ae57c02ac46da3a28c0da05c771157a10b79cc8
maivantai2003 <vantai08122003@gmail.com>
2025-12-04 01:09:46 +0700

    chore(docker-compose): update RabbitMQ & Redis ports for VPS deploy

---
+++ chatgroup-server/entrypoint.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+set -e
+
+echo "Waiting for SQL Server..."
+sleep 10
+
+echo "Running migrations..."
+dotnet ef database update
+
+echo "Starting application..."
+dotnet chatgroup-server.dll
